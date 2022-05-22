76b14b013b4b376bef7b64cc2d8c6aa7b55cbbbf
Khizri Makhmudov <khizrim@khizrim.ru>
2022-05-22 09:07:11 +0300

    feat: fetching data from server

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -4,17 +4,45 @@ import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 
-import { data, types } from '../../utils/data';
+import { types } from '../../utils/data';
 
 import styles from './app.module.css';
+import LoadingSpinner from '../loading-spinner/loading-spinner';
 
 function App() {
+  const [ingredients, setIngredients] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [error, setError] = React.useState('');
+
+  const url = 'https://norma.nomoreparties.space/api/ingredients';
+
+  React.useEffect(() => {
+    setIsLoading(true);
+    fetch(url)
+      .then((res) => (res.ok ? res.json() : Promise.reject(res.status)))
+      .then(({ data }) => setIngredients(data))
+      .catch((err) => setError(String(err)))
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <div className={styles.app}>
       <AppHeader />
+      {isLoading && <LoadingSpinner />}
+      {error && (
+        <div className={styles.error}>
+          Что-то пошло не так. Ошибка: <span className={styles.error_text}>{error}</span>
+        </div>
+      )}
       <main className={styles.main}>
-        <BurgerIngredients data={data} types={types} />
-        <BurgerConstructor data={data} />
+        {ingredients.length ? (
+          <>
+            <BurgerIngredients data={ingredients} types={types} />
+            <BurgerConstructor data={ingredients} />
+          </>
+        ) : (
+          ''
+        )}
       </main>
     </div>
   );
